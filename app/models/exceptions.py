f5fd06b57ca4c4dcb011c6d53b62e78aeb577114
Paolo Billordo <paolobillordo@gmail.com>
2023-09-05 23:11:58 -0300

    archivos

---
+++ app/models/exceptions.py
@@ -0,0 +1,20 @@
+from flask import jsonify
+
+class CustomException(Exception):
+
+    def __init__(self, status_code, name = "Custom Error", description = 'Error'): 
+        super().__init__()
+        self.description = description
+        self.name = name
+        self.status_code = status_code
+
+    def get_response(self):
+        response = jsonify({
+            'error': {
+                'code': self.status_code,
+                'name': self.name,
+                'description': self.description,
+            }
+        })
+        response.status_code = self.status_code
+        return response
